9a36fa482960c27bfec7edc308041e9ff3ee81d6
Duc Dang <mrtrinh5293@gmail.com>
2018-04-30 00:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newItem = new Object({itemName: item, itemPrice: Math.floor(Math.random() * 100)});
+ cart.push(newItem);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have ";
+  if ( cart.length === 0){
+    return "Your shopping cart is empty.";
 }
-
+  // for ( var i = 0; i < cart.length; i++){
+    
+  if ( cart.length === 1){
+  return `${message}${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    }
+      
+  if ( cart.length === 2 ){
+  return `${message}${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+      }
+      
+  else{
+        var newCart = [];
+        for ( var i = 2; i < cart.length; i++){
+     newCart.push(`, and ${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+ }
+}
 function total() {
   // write your code here
 }
