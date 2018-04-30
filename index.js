766cd7d91f292adbb0dcdc4a34bd3191a042e0e6
Duc Dang <mrtrinh5293@gmail.com>
2018-04-30 00:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ function removeFromCart(item) {
       cart.splice(i,1);
       return cart
     }
-    // else {
+    else {
+      return "That item is not in your cart.";
     }
   }
-      return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
